950511b8642f9a0adfa2b0a163b487b056e306a4
shloimiminkowicz <shloimimink@gmail.com>
2020-12-16 14:25:01 -0600

    initial final commit react

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react'
+import Launches from "./components/Launches";
+import Launch from "./components/Launch";
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+// Apollo
+import ApolloClient from 'apollo-boost'
+import {ApolloProvider} from 'react-apollo'
+import logo from './logo.png'
+
+const client = new ApolloClient({
+    uri: '/graphql'
+})
+
+class App extends Component {
+    render() {
+        return (
+            <ApolloProvider client={client}>
+                <Router>
+                    <div className="container">
+                        <img
+                            src={logo}
+                            alt="SpaceX"
+                            style={{width: 300, display: 'block', margin: 'auto'}}/>
+                        <Route exact path="/" component={Launches}/>
+                        <Route exact path="/launch/:flight_number" component={Launch}/>
+                    </div>
+                </Router>
+            </ApolloProvider>
+
+        );
+    }
+}
+
+export default App;
